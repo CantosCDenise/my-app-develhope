48c8b0afdc69380760627beb45603ba2b4220148
CantosCDenise <denisecantos2905@gmail.com>
2024-01-07 11:16:08 +0100

    Handling Events - 2

--- src/MouseClicker.jsx
+++ src/MouseClicker.jsx
@@ -2,7 +2,13 @@ export function MouseClicker(){
     function handleButtonClick(event){
         console.log(event.target.name)
     }
+    function handleImageClick(event){
+        console.log(event.target.src)
+    }
     return (
-        <button name="one" onClick={handleButtonClick}>Click me!</button>
+        <button name="one" onClick={handleButtonClick}>
+            <img onClick={handleImageClick} src="https://i.pinimg.com/564x/d8/84/b0/d884b0c43aa1cc1d4f1549f7b0abf461.jpg" width={50} height={50} />
+            Click me!
+            </button>
     )
 }
