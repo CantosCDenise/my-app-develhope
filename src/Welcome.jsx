2528781e4674c06c388c43d217ae8c5cbc3248c4
CantosCDenise <denisecantos2905@gmail.com>
2024-01-08 08:42:18 +0100

    Conditional Rendering - 1

--- src/Welcome.jsx
+++ src/Welcome.jsx
@@ -4,9 +4,9 @@ export function Welcome({ name, age = 24 }){
     return <div>
         <strong> Welcome, {name}!</strong>
         <p><Age age={age}/></p>
-        {age >=18 && <p><Age age={age}/></p>}
+        {age > 18 && <p><Age age={age}/></p>}
         {age && <p><Age age={age}/></p>}
-        {age >= 18 && age <= 65 && <p><Age age={age}/></p>}
-        {age >= 18 && age <= 65 && name === "John" && <p><Age age={age}/></p>}
+        {age > 18 && age < 65 && <p><Age age={age}/></p>}
+        {age > 18 && age < 65 && name === "John" && <p><Age age={age}/></p>}
         </div>
 }
