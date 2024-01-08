3252d76b6c3441ff9abf7ea102bcc908d5559ce3
CantosCDenise <denisecantos2905@gmail.com>
2024-01-08 08:54:27 +0100

    State - 2

--- src/Counter.jsx
+++ src/Counter.jsx
@@ -1,15 +1,26 @@
 import { useState } from "react";
+import { CounterDisplay } from "./CounterDisplay";
 
-export function Counter({ initialValue = 0}){
-    const [counter, setCounter] = useState(initialValue)
+export function Counter({ initialValue = 0, amount = 1}){
+    const [count, setCounter] = useState(initialValue)
     
     function handleCounterIncrement(){
-        setCounter((c) => c + 1)
+        setCounter((c) => c + amount)
+    }
+    
+    function handleCounterDecrement(){
+        setCounter((c) => c - amount)
+    }
+
+    function handleCounterReset(){
+        setCounter((c) => c = initialValue)
     }
     return (
         <div>
-            <h2>{counter}</h2>
+            <CounterDisplay count={count}/>
             <button onClick={handleCounterIncrement}>Increment</button>
+            <button onClick={handleCounterDecrement}>Decrement</button>
+            <button onClick={handleCounterReset}>Reset</button>
         </div>
     )
 }
